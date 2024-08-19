038e14f64437da6397d0afba9d42b8aab5ee2c6b
lelong2507 <hoanglong6939tcv@gmail.com>
2024-08-19 18:13:17 +0700

    Assignment-check-2

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -10,7 +10,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
